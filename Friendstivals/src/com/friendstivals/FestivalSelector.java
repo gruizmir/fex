119cf04a5dfb9180aebce86b027dabf4734be172
Astom <zetwg@hotmail.com>
2012-10-07 16:15:35 -0400

    saasdas

--- Friendstivals/src/com/friendstivals/FestivalSelector.java
+++ Friendstivals/src/com/friendstivals/FestivalSelector.java
@@ -1,21 +1,37 @@
 package com.friendstivals;
 
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Window;
-import android.widget.LinearLayout;
+import java.util.ArrayList;
 
-public class FestivalSelector extends ListActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.friendstivals.utils.Event;
+
+public class FestivalSelector extends Activity {
+
+	private ListView listView1;
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.festival_selector);
-        
-        //LayoutInflater factory = LayoutInflater.from(this.getApplicationContext()); 
-        //LinearLayout festivalView = (LinearLayout)factory.inflate( R.layout.festival_list_item, null);
-        
-        //this.setListAdapter(adapter);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.festival_selector);
+
+		ArrayList<Event> eventos = new ArrayList<Event>();
+		
+		Event evento = new Event(null, null, null, null, null);
+		
+		eventos.add(evento);
+		
+		FestivalAdapter adapter = new FestivalAdapter(this,
+                R.layout.festival_list_item, eventos);
+		
+		listView1 = (ListView)findViewById(R.id.list_festival);
+
+		View header = (View)getLayoutInflater().inflate(R.layout.festival_list_item, null);
+		listView1.addHeaderView(header);
+
+		listView1.setAdapter(adapter);
+	}
 }

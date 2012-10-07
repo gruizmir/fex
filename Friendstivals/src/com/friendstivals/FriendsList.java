5ae9645e62410cdcacff3be22c9b71001e7a2181
Gabriel Ruiz <gabriel.ruiz@skyvortex.cl>
2012-10-07 15:09:02 -0300

    Se agrego la vista de lista de amigos que usan la app friendstivals. Falta arreglar el codigo para que sea coherente con las clases (esta full desorden)

--- Friendstivals/src/com/friendstivals/FriendsList.java
+++ Friendstivals/src/com/friendstivals/FriendsList.java
@@ -1,15 +1,134 @@
 package com.friendstivals;
 
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.view.Window;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class FriendsList extends ListActivity {
-	
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.friendstivals.utils.FriendsGetProfilePics;
+import com.friendstivals.utils.Utility;
+
+public class FriendsList extends Activity implements OnItemClickListener {
+	private Handler mHandler;
+
+	protected ListView friendsList;
+	protected static JSONArray jsonArray;
+	protected String graph_or_fql;
+
+	/*
+	 * Layout the friends' list
+	 */
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.friend_list);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		mHandler = new Handler();
+		setContentView(R.layout.friend_list);
+
+		Bundle extras = getIntent().getExtras();
+		String apiResponse = extras.getString("API_RESPONSE");
+		try {
+			jsonArray = new JSONArray(apiResponse);
+		} catch (JSONException e) {
+			return;
+		}
+		friendsList = (ListView) findViewById(R.id.amigos);
+		friendsList.setOnItemClickListener(this);
+		friendsList.setAdapter(new FriendListAdapter(this));
+
+	}
+
+	/*
+	 * Clicking on a friend should popup a dialog for user to post on friend's
+	 * wall.
+	 */
+	public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
+		try {
+			final long friendId;
+			friendId = jsonArray.getJSONObject(position).getLong("uid");
+			String name = jsonArray.getJSONObject(position).getString("name");
+
+		} catch (JSONException e) {
+		}
+	}
+
+	/**
+	 * Definition of the list adapter
+	 */
+	public class FriendListAdapter extends BaseAdapter {
+		private LayoutInflater mInflater;
+		FriendsList friendsList;
+
+		public FriendListAdapter(FriendsList friendsList) {
+			this.friendsList = friendsList;
+			if (Utility.model == null) {
+				Utility.model = new FriendsGetProfilePics();
+			}
+			Utility.model.setListener(this);
+			mInflater = LayoutInflater.from(friendsList.getBaseContext());
+		}
+
+		public int getCount() {
+			return jsonArray.length();
+		}
+
+		public Object getItem(int position) {
+			return null;
+		}
+
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			JSONObject jsonObject = null;
+			try {
+				jsonObject = jsonArray.getJSONObject(position);
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			View hView = convertView;
+			if (convertView == null) {
+				hView = mInflater.inflate(R.layout.friend_list_item, null);
+				ViewHolder holder = new ViewHolder();
+				holder.profile_pic = (ImageView) hView.findViewById(R.id.friend_list_photo);
+				holder.name = (TextView) hView.findViewById(R.id.friend_list_name);
+				//                holder.info = (TextView) hView.findViewById(R.id.info);
+				hView.setTag(holder);
+			}
+
+			ViewHolder holder = (ViewHolder) hView.getTag();
+			try {
+				holder.profile_pic.setImageBitmap(Utility.model.getImage(
+						jsonObject.getString("uid"), jsonObject.getString("pic_square")));
+			} catch (JSONException e) {
+			}
+			try {
+				holder.name.setText(jsonObject.getString("name"));
+			} catch (JSONException e) {
+				holder.name.setText("");
+			}
+			return hView;
+		}
+
+	}
+
+	class ViewHolder {
+		ImageView profile_pic;
+		TextView name;
+	}
 }
--- Friendstivals/src/com/friendstivals/Friendstivals.java
+++ Friendstivals/src/com/friendstivals/Friendstivals.java
@@ -4,10 +4,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +26,11 @@ import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
 import com.facebook.android.Facebook.DialogListener;
 import com.facebook.android.FacebookError;
+import com.facebook.android.R;
+import com.facebook.android.Util;
+import com.friendstivals.utils.BaseRequestListener;
+import com.friendstivals.utils.SessionStore;
+import com.friendstivals.utils.Utility;
 
 public class Friendstivals extends Activity {
 
@@ -30,6 +39,9 @@ public class Friendstivals extends Activity {
 	private SharedPreferences mPrefs;
 	private Button fbButton; 
 	private Button mailButton;
+	private Handler mHandler;
+	public String userUID = null;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -40,34 +52,121 @@ public class Friendstivals extends Activity {
 		fbButton.setClickable(false);
 		mailButton = (Button) this.findViewById(R.id.mail_button);
 		mailButton.setClickable(false);
-		/*
-		 * Get existing access_token if any
-		 */
-		mPrefs = getPreferences(MODE_PRIVATE);
-		String access_token = mPrefs.getString("access_token", null);
-		long expires = mPrefs.getLong("access_expires", 0);
-		if(access_token != null) {
-			facebook.setAccessToken(access_token);
-		}
-		if(expires != 0) {
-			facebook.setAccessExpires(expires);
-		}
+		mHandler = new Handler();
 
-		/*
-		 * Only call authorize if the access_token has expired.
-		 */
-		if(!facebook.isSessionValid()) {
-			fbButton.setClickable(true);
+//		// Create the Facebook Object using the app id.
+//		Utility.mFacebook = new Facebook(getString(R.string.fb_id));
+//		// Instantiate the asynrunner object for asynchronous api calls.
+//		Utility.mAsyncRunner = new AsyncFacebookRunner(Utility.mFacebook);
+
+
+		// restore session if one exists
+		SessionStore.restore(facebook, this);
+
+		if (facebook.isSessionValid()) {
+//			requestUserData();
+			openFestivalList();
 		}
 		else{
-			openFestivalList();
+			fbButton.setClickable(true);
+		}
+
+
+		//		/*
+		//		 * Get existing access_token if any
+		//		 */
+//				mPrefs = getPreferences(MODE_PRIVATE);
+//				String access_token = mPrefs.getString("access_token", null);
+//				long expires = mPrefs.getLong("access_expires", 0);
+		//		if(access_token != null) {
+		//			facebook.setAccessToken(access_token);
+		//		}
+		//		if(expires != 0) {
+		//			facebook.setAccessExpires(expires);
+		//		}
+
+		//		/*
+		//		 * Only call authorize if the access_token has expired.
+		//		 */
+		//		if(!facebook.isSessionValid()) {
+		//			fbButton.setClickable(true);
+		//		}
+		//		else{
+		//			openFestivalList();
+		//		}
+
+	}
+
+	/*
+	 * Request user name, and picture to show on the main screen.
+	 */
+	public void requestUserData() {
+		Bundle params = new Bundle();
+		params.putString("fields", "name, picture");
+		mAsyncRunner.request("me", params, new UserRequestListener());
+	}
+
+	/*
+	 * The Callback for notifying the application when authorization succeeds or
+	 * fails.
+	 */
+
+	/*
+	 * Callback for fetching current user's name, picture, uid.
+	 */
+	public class UserRequestListener extends BaseRequestListener {
+
+		public void onComplete(final String response, final Object state) {
+			JSONObject jsonObject;
+			try {
+				jsonObject = new JSONObject(response);
+				userUID = jsonObject.getString("id");
+
+				mHandler.post(new Runnable() {
+					public void run() {
+						//                        mText.setText("Welcome " + name + "!");
+						//                        mUserPic.setImageBitmap(Utility.getBitmap(picURL));
+					}
+				});
+
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 	}
 
 	private void openFestivalList() {
-		Intent myIntent = new Intent(this, FestivalSelector.class);
-		this.startActivity(myIntent);		
+		//		Intent myIntent = new Intent(this, FestivalSelector.class);
+		if (!facebook.isSessionValid()) {
+			Util.showAlert(this, "Warning", "You must first log in.");
+		} else {
+			String query = "select name, current_location, uid, pic_square from user where uid in (select uid2 from friend where uid1=me()) and is_app_user='true' order by name";
+			Bundle params = new Bundle();
+			params.putString("method", "fql.query");
+			params.putString("query", query);
+			mAsyncRunner.request(null, params,
+					new FriendsRequestListener());
+
+		}
+	}
+
+	/*
+	 * callback after friends are fetched via me/friends or fql query.
+	 */
+	public class FriendsRequestListener extends BaseRequestListener {
+
+		public void onComplete(final String response, final Object state) {
+			Intent myIntent = new Intent(getApplicationContext(), FriendsList.class);
+			myIntent.putExtra("API_RESPONSE", response);
+			startActivity(myIntent);
+		}
+
+		public void onFacebookError(FacebookError error) {
+			Toast.makeText(getApplicationContext(), "Facebook Error: " + error.getMessage(),
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
@@ -117,32 +216,30 @@ public class Friendstivals extends Activity {
 	}
 
 	public void fbLogin(View v){
-		facebook.authorize(this, new String[] {"publish_checkins"},new DialogListener() {
+		facebook.authorize(this, new String[] {"offline_access","publish_checkins"},new DialogListener() {
 			public void onComplete(Bundle values) {
-				SharedPreferences.Editor editor = mPrefs.edit();
-				editor.putString("access_token", facebook.getAccessToken());
-				editor.putLong("access_expires", facebook.getAccessExpires());
-				editor.commit();
+				SessionStore.save(facebook, Friendstivals.this);
+				requestUserData();
 				fbButton.setClickable(false);
 				openFestivalList();
 			}
 
 			public void onFacebookError(FacebookError error) {
-				Toast.makeText(Friendstivals.this, error.getMessage() , Toast.LENGTH_SHORT).show();
+				Toast.makeText(Friendstivals.this, error.getMessage() , Toast.LENGTH_LONG).show();
 			}
 
 			public void onError(DialogError e) {
-				Toast.makeText(Friendstivals.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+				Toast.makeText(Friendstivals.this, e.getMessage(), Toast.LENGTH_LONG).show();
 			}
 
 			public void onCancel() {
-				Toast.makeText(Friendstivals.this, "Cancelado", Toast.LENGTH_SHORT).show();
+				Toast.makeText(Friendstivals.this, "Cancelado", Toast.LENGTH_LONG).show();
 			}
 		});
 	}
-	
+
 	public void onResume() {    
-        super.onResume();
-        facebook.extendAccessTokenIfNeeded(this, null);
-    }
+		super.onResume();
+		facebook.extendAccessTokenIfNeeded(this, null);
+	}
 }

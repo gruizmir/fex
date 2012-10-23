18ff50117e0362e6c8a1a93f9e97199ef0f86aa5
Astom <astom@ubuntu.ubuntu-domain>
2012-10-23 17:55:13 -0300

    nuevas vistas

--- Friendstivals/src/com/friendstivals/InviteView.java
+++ Friendstivals/src/com/friendstivals/InviteView.java
@@ -1,7 +1,16 @@
 package com.friendstivals;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
 
-public class InviteView extends Activity{
+public class InviteView extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.festival_invite_list);
+	}
 
 }
